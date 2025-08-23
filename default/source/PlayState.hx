1ac212660a98bcd387c09bf681c28b152ecf9ad7
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-23 08:57:07 -0500

    build text in the corner

--- default/source/PlayState.hx
+++ default/source/PlayState.hx
@@ -1,11 +1,16 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import flixel.text.FlxText;
+import lime.app.Application;
 
 class PlayState extends FlxState
 {
 	override public function create()
 	{
+		add(new FlxText(10, 10, FlxG.width, '${Application.current.meta.get('file')}.${Main.BUILD}'));
+
 		super.create();
 	}
 
